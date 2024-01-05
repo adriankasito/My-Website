151acb831a007f3f039a4a5d73906a3cc465fd8b
adriankasito <67113930+adriankasito@users.noreply.github.com>
2024-01-05 16:57:25 +0300

    Update LoaderLogo.js

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./LoaderLogo.css";
-import logo from "../../assets/logo/logo-01.png";
+import logo from "../../assets/logo/logo.png";
 class LogoLoader extends React.Component {
   render() {
     return <img src={logo} alt="logo" id="logo" />;
