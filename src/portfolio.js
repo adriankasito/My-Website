c24b601011978d2df50953f4e31eccf4ff74b97e
Adrian Kasito <adriankc910@gmail.com>
2024-09-14 02:44:21 +0300

    changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -19,7 +19,7 @@ const greeting = {
   title: "Hello!",
   sub: "Adrian Kasito",
   logo_name: "Adrian Kasito",
-  resumeLink: "https://drive.google.com/file/d/13PRFpLcsp3Tiw8DL3qz_sTkhqZmlcPPO/view?usp=drive_link",
+  resumeLink: "https://docs.google.com/document/d/1zbe8UM39fpDA8q-2VjpHreI7eVsBXTD2nQqzPhWieqQ/edit?usp=drive_link",
 };
 
 // home logos
